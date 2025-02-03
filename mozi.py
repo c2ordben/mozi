b7da34f25c6da096dbbf90565c4a5c07a55893e8
c2ordben <c2ordben@vasvari.org>
2025-02-03 09:36:02 +0100

    c

--- mozi.py
+++ mozi.py
@@ -1,23 +1,58 @@
 import random
 ar=[2500, 2100, 1300, 0]
-jegy = 6
+nezoterek=[]
+a = int(input())
+
 def nezoter():
     for i in range(15):
-        kislista=[]
-        for j in range(10):
-            kislista.append(random.choice(ar))
-        kislista.append('|')
-        for j in range(10):
-            kislista.append(random.choice(ar))
-        print(kislista)
+        sorok=[]
+        for i in range(10):
+            sorok.append(random.choice(ar))
+        sorok.append('____')
+        for i in range(10):
+            sorok.append(random.choice(ar))
+        print(sorok)
+        nezoterek.append(sorok)
+
+
+def bevetel(nezoterek):
+    osszeg = 0
+    for i in range(len(nezoterek)):
+        for j in range(len(nezoterek[i])):
+            if nezoterek[i][j] == 0:
+                osszeg += 0
+            if nezoterek[i][j] == 1300:
+                osszeg += 1300
+            if nezoterek[i][j] == 2100:
+                osszeg += 2100
+            if nezoterek[i][j] == 2500:
+                osszeg += 3500
+    print(osszeg, 'az összes bevétel')
+
+
+def TeremKihasznaltsag(nezoterek):
+    szabadHely = 300
+    for i in range(len(nezoterek)):
+        for j in range(len(nezoterek[i])):
+            if nezoterek[i][j] == 0:
+                szabadHely -= 1
+    print(round((szabadHely / 300)*100, 2),'%','hely foglalt')
+
+
+def FelnottJegyek(nezoterek):
+    felnott = 0
+    for i in range(len(nezoterek)):
+        for j in range(len(nezoterek[i])):
+            if nezoterek[i][j] == 2500:
+                felnott += 1
+    print(felnott,'felnőtt jegyet vettek')
+
+
+def JegyekSzama():
+    if 
+
+
 nezoter()
-def jegyek():
-    print('Mennyi jegyet szeretne?')
-    a = int(input())
-    while a < 2 and a > 5 and 1 < a > 6:
-        if a > 5:
-            print('maximum 5 jegyet lehet vásárolni')
-        elif a < 2:
-            print('minimum 2 jegyet kell venni')
-        else:
-jegyek()
+bevetel(nezoterek)
+TeremKihasznaltsag(nezoterek)
+FelnottJegyek(nezoterek)
