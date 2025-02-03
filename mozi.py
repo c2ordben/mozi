bec3bab065d3530b1c45d6dc67765df70b4adcfd
c2ordben <c2ordben@vasvari.org>
2025-02-03 12:40:20 +0100

    a

--- mozi.py
+++ mozi.py
@@ -1,7 +1,14 @@
 import random
+
 ar=[2500, 2100, 1300, 0]
 nezoterek=[]
+print('hány jegyet szeretne? (2-5)')
 a = int(input())
+
+while a > 5 or a < 2:
+    print('2-5db jegyet válasszon!')
+    a = int(input())
+
 
 def nezoter():
     for i in range(15):
@@ -45,14 +52,49 @@ def FelnottJegyek(nezoterek):
         for j in range(len(nezoterek[i])):
             if nezoterek[i][j] == 2500:
                 felnott += 1
-    print(felnott,'felnőtt jegyet vettek')
+    print(felnott,'db teljes jegyet vettek')
 
 
-def JegyekSzama():
-    if 
+def JegySzam(a):
+    if a == 2:
+        sor = 0
+        for i in range(len(nezoterek)):
+            for j in range(len(nezoterek[i])):
+                    if nezoterek[i][j] == 0 and nezoterek[i][j-1] == 0:
+                        sor = i+1
+                        print(sor, '. sorban van 2 hely')
+                        return
+    if a == 3:
+        sor = 0
+        for i in range(len(nezoterek)):
+            for j in range(len(nezoterek[i])):
+                    if nezoterek[i][j] == 0 and nezoterek[i][j-1] == 0 and nezoterek[i][j-2] == 0:
+                        sor = i+1
+                        print(sor, '. sorban van 3 hely')
+                        return
+    if a == 4:
+        sor = 0
+        for i in range(len(nezoterek)):
+            for j in range(len(nezoterek[i])):
+                    if nezoterek[i][j] == 0 and nezoterek[i][j-1] == 0 and nezoterek[i][j-2] == 0 and nezoterek[i][j-3] == 0:
+                        sor = i+1
+                        print(sor, '. sorban van 4 hely')
+                        return
+    if a == 5:
+        sor = 0
+        for i in range(len(nezoterek)):
+            for j in range(len(nezoterek[i])):
+                    if nezoterek[i][j] == 0 and nezoterek[i][j-1] == 0 and nezoterek[i][j-2] == 0 and nezoterek[i][j-3] == 0 and nezoterek[i][j-4] == 0:
+                        sor = i+1
+                        print(sor, '. sorban van 5 hely')
+                        return
+    print('nincs')
+
 
 
 nezoter()
+JegySzam(a)
 bevetel(nezoterek)
 TeremKihasznaltsag(nezoterek)
 FelnottJegyek(nezoterek)
+
